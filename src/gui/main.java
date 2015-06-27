ab1c283bfe8ec079a7c572d5cbcc95095731f214
Jarek606 <jarekdawidowicz+github@gmail.com>
2015-06-27 12:49:39 +0200

    test122

--- src/gui/main.java
+++ src/gui/main.java
@@ -48,7 +48,7 @@ public class main extends JFrame {
 	final static String extension = "extension_gui.bin";
 
 	/**
-	 * Launch the application.asa
+	 * Launch the application.
 	 */
 	public static void main(String[] args) {
 		
